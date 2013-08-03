6e4ddcee9265be98febe50d6ca85b527568afc37
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-08-03 17:14:41 -0400

    testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happyhour::Application.config.secret_token = '7eed780be4008a6eb80a2b4ab6e5a4b4c848f2ef747fd2c3f9749b424148f525ea148ee1e62f78f8524c61ce88eb7a45f81b86df35775551f44db40e6a6486e9'
